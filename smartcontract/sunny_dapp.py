8b3281e3e97f23588f7456e4b96f3136b6843321
Zorrrow <110273869+Zorrrow@users.noreply.github.com>
2020-05-09 12:20:07 +0000

    simplify travis

--- smartcontract/sunny_dapp.py
+++ smartcontract/sunny_dapp.py
@@ -2,4 +2,7 @@
 Insurance dApp
 ===================================
 
-This dApp is an exam
+This dApp is an example of an insurance contract that pays out in case of
+an external event, that is signalled by an oracle.
+
+It

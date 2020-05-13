cd9b01b710ac7a6bfa9584806ef96d5f539857d9
Zorrrow <110273869+Zorrrow@users.noreply.github.com>
2020-05-13 18:58:24 +0000

    Update Release Notes

--- smartcontract/sunny_dapp.py
+++ smartcontract/sunny_dapp.py
@@ -7,4 +7,6 @@ an external event, that is signalled by an oracle.
 
 It has the following entities: the owner, customer, insurer (can be the owner
 or a third party) and the oracle. The owner can initialize an agreement.
-The oracle signals the result of the event, after which the cu
+The oracle signals the result of the event, after which the customer,
+insurer or owner can claim to pay out the insured amount/premium.
+The oracle will also be paid for its s

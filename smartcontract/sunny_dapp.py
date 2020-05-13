b497bad8cf5be619852404c356e45a0b5b269701
Zorrrow <110273869+Zorrrow@users.noreply.github.com>
2020-05-13 15:01:29 +0000

    Revert "Update README"

--- smartcontract/sunny_dapp.py
+++ smartcontract/sunny_dapp.py
@@ -6,4 +6,5 @@ This dApp is an example of an insurance contract that pays out in case of
 an external event, that is signalled by an oracle.
 
 It has the following entities: the owner, customer, insurer (can be the owner
-or a third party) and the oracle. The
+or a third party) and the oracle. The owner can initialize an agreement.
+The oracle signals the result of the event, after which the cu

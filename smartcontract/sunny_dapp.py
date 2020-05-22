535eb40609231652b007b64d6f4bfeab310099b3
Zorrrow <110273869+Zorrrow@users.noreply.github.com>
2020-05-22 11:55:49 +0000

    Unreviewed gardening.

--- smartcontract/sunny_dapp.py
+++ smartcontract/sunny_dapp.py
@@ -9,4 +9,6 @@ It has the following entities: the owner, customer, insurer (can be the owner
 or a third party) and the oracle. The owner can initialize an agreement.
 The oracle signals the result of the event, after which the customer,
 insurer or owner can claim to pay out the insured amount/premium.
-The oracle will also be paid for its s
+The oracle will also be paid for its services/costs.
+
+In case the oracle fails to signal the result, the owner can refund bo
